71d702a76178bf21d5a295160700cc79e31b2a5c
AndersonDay <anderson.day@target.com>
2020-03-18 15:23:01 -0500

    saving and testing

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+
+const Home = () => (
+  <div className="container">
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <header>
+      hey
+    </header>
+
+    <main>
+      main
+    </main>
+
+    <footer>
+      footer
+    </footer>
+  </div>
+)
+
+export default Home
