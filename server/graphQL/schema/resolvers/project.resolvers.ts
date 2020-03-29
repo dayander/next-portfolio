e0592551da307f2b1d5f2eb0cb9cefaa324cc8ff
AndersonDay <anderson.day@target.com>
2020-03-29 16:46:18 -0400

    cleaning default return

--- server/graphQL/schema/resolvers/project.resolvers.ts
+++ server/graphQL/schema/resolvers/project.resolvers.ts
@@ -3,21 +3,20 @@ interface ProjectInput {
     projectName?: string;
     address?: string;
     getter?: string;
-  }
+  };
 }
 
 export default {
   Query: {
-    project: (_: any, {input}: ProjectInput, ctx: any) => {
-      if(input.address){
-        return ctx.models.Project.findOne({'address': input.address });
+    project: (_: any, { input }: ProjectInput, ctx: any) => {
+      if (input.address) {
+        return ctx.models.Project.findOne({ address: input.address });
       }
-      if(input.getter){
-        return ctx.models.Project.findOne({'getter': input.getter });
+      if (input.getter) {
+        return ctx.models.Project.findOne({ getter: input.getter });
       }
-      const p = ctx.models.Project.findOne({'projectName': input.projectName });
-      console.log(p)
-      return p
-    }
-  }
+
+      return ctx.models.Project.findOne({ projectName: input.projectName });
+    },
+  },
 };
