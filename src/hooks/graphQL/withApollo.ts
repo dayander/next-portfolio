70be6e0cbb46ecdb5c04351260ad7c2443c073d6
AndersonDay <anderson.day@target.com>
2020-03-29 16:49:07 -0400

    HOC for gql server render

--- src/hooks/graphQL/withApollo.ts
+++ src/hooks/graphQL/withApollo.ts
@@ -1,10 +1,19 @@
 import withApollo from 'next-with-apollo';
-import ApolloClient, { InMemoryCache } from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
+import fetch from 'isomorphic-unfetch';
+import { createHttpLink } from 'apollo-link-http';
+
+const link = createHttpLink({
+  fetch, // Switches between unfetch & node-fetch for client & server.
+  uri: `http://localhost:${process.env.endpoint ?? 3000}/api/graphql`,
+});
 
 export default withApollo(
   ({ initialState }) =>
     new ApolloClient({
-      uri: `http://localhost:${process.env.endpoint ?? 3000}/api/graphql`,
-      cache: new InMemoryCache().restore(initialState || {}),
+      link,
+      cache: new InMemoryCache().restore(initialState),
+      ssrMode: true,
     })
 );
